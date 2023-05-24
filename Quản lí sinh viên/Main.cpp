f9db674eeeed637fbf94af58f574980c423f7d53
MinhDuy <minhduy1401ntmk@gmail.com>
2023-05-24 21:23:14 +0700

    edit

--- Quản lí sinh viên/Main.cpp
+++ Quản lí sinh viên/Main.cpp
@@ -1,8 +1,5 @@
 #include"ConsoleScreen.h"
 #include"Global.h"
 int main() {
-	getCurrentDate();
-	createSchoolYear();
-	ifstream in("./Data/" + currentSchoolYear + "/semester.txt");
-	in.close();
+	
 }
