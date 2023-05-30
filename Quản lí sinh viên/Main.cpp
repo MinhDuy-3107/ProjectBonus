ec012f351d99e333a6071482c187aa12e7a25025
MinhDuy <minhduy1401ntmk@gmail.com>
2023-05-30 21:40:22 +0700

    add func

--- Quản lí sinh viên/Main.cpp
+++ Quản lí sinh viên/Main.cpp
@@ -3,18 +3,5 @@
 
 
 int main() {
-	int width = 8, top = 8, height = 4, left = 8;
-	gotoXY(left, top);
-	cout << char(201);
-	for (int i = 0; i < width; i++) {
-		cout << char(205);
-	}
-	cout << char(187);
-	for (int i = top + 1; i < height + top; i++) {
-		gotoXY(left, i);
-		cout << char(186);
-	}
-	
-	
-	
+		
 }
